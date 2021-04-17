ccc31bbb29a47bfb6b9af2bb9df79772c4e21bd9
Humza Hasan <humzahasan2101@gmail.com>
2021-04-17 13:36:49 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [subitem, setSubItem] = useState({});
+  const foodMenu = {
+    dessert: { cheesecake: "4/5", "ice cream": "5/5" },
+    drinks: { "virgin mojito": "4/5", "KFC Krusher": "5/5" },
+    fastfood: { burger: "4/5", "French Fries": "5" }
+  };
+  const cuisines = Object.keys(foodMenu);
+  //console.log(cuisines);
+
+  const getSubMenu = (list) => {
+    setSubItem(foodMenu[list]);
+  };
+
+  //console.log(subitem);
+  return (
+    <div className="App">
+      {cuisines.map((cuisine) => (
+        <span key={cuisine} onClick={() => getSubMenu(cuisine)}>
+          {cuisine}{" "}
+        </span>
+      ))}
+      <div className="app-items">
+        {Object.keys(subitem).map((key, i) => (
+          <div className="app-item" key={i}>
+            <p>{key}</p>
+            <small>Rating : {subitem[key]}</small>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
